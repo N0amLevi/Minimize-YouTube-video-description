5bc434232a317b39f598ea3c921a075d901f5419
Noam Levi <112897999+N0amLevi@users.noreply.github.com>
2024-06-22 13:42:15 +0300

    Create Minimize video descriptions-1.0.0.user.js

---
+++ Minimize video descriptions-1.0.0.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Minimize video descriptions
+// @namespace    https://github.com/N0amLevi/Minimize-YouTube-video-description
+// @version      1.0.0
+// @description  Minimize the description of youtube videos automaticlly
+// @author       Noam Levi
+// @match        http://*/*
+// @grant        none
+
+// @domain          youtube.com
+// @include         http*://www.youtube.com/*
+// @include         http*://m.youtube.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector(`#collapse`).click();
+})();
